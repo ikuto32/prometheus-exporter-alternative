6f0f79abbf53be7e2a91047b3d020ee12554d2f7
ikuto32 <47182163+ikuto32@users.noreply.github.com>
2026-07-09 22:14:20 +0900

    Update SwitchBot meter advertisement parsing

--- PrometheusExporter.Instrumentation.SwitchBot.Linux/SwitchBotAdvertisementParser.cs
+++ PrometheusExporter.Instrumentation.SwitchBot.Linux/SwitchBotAdvertisementParser.cs
@@ -28,4 +28,53 @@ internal static class SwitchBotAdvertisementParser
 
         return true;
     }
+
+    internal static bool TryDecodeMeterTemperatureHumidity(
+        ReadOnlySpan<byte> serviceData,
+        ReadOnlySpan<byte> manufacturerData,
+        out double temperature,
+        out double humidity)
+    {
+        if (manufacturerData.Length >= 11)
+        {
+            return TryDecodeTemperatureHumidity(manufacturerData[8..11], out temperature, out humidity);
+        }
+
+        if (serviceData.Length >= 6)
+        {
+            return TryDecodeTemperatureHumidity(serviceData[3..6], out temperature, out humidity);
+        }
+
+        temperature = double.NaN;
+        humidity = double.NaN;
+        return false;
+    }
+
+    internal static bool TryDecodeMeterProCo2(
+        ReadOnlySpan<byte> serviceData,
+        ReadOnlySpan<byte> manufacturerData,
+        out double temperature,
+        out double humidity,
+        out double co2)
+    {
+        co2 = double.NaN;
+
+        if (!TryDecodeMeterTemperatureHumidity(serviceData, manufacturerData, out temperature, out humidity))
+        {
+            return false;
+        }
+
+        if (manufacturerData.Length < 15)
+        {
+            return true;
+        }
+
+        var value = (manufacturerData[13] << 8) | manufacturerData[14];
+        if (value <= 9999)
+        {
+            co2 = value;
+        }
+
+        return true;
+    }
 }
